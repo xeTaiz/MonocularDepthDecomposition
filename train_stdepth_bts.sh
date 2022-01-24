658a90d8f0b82a06a4819d826581771f74894948
Dominik Engel <dominik.engel@uni-ulm.de>
2022-01-24 16:15:22 +0100

    update train scripts

---
+++ train_stdepth_bts.sh
@@ -0,0 +1,1 @@
+python mono-depth-estimation/train.py --min_epochs 10 --max_epochs 50 --gpus -1 --worker 6 bts --learning_rate 1e-5 --batch_size 16 --loss mse stdepth --path '/mnt/hdd/dome/STDepth_data' --training --validation
