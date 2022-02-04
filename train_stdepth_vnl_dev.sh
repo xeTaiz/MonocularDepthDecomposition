62af0a172702897ea47a5dc0c948a5f88b45a8db
Dominik Engel <dominik.engel@mail.de>
2022-02-04 09:45:12 +0100

    update submit vnl_all

--- train_stdepth_vnl_dev.sh
+++
@@ -1,2 +0,0 @@
-python mono-depth-estimation/train.py --max-train-batches 10 --precision 32 --max_epochs 2 --min_epochs 1 --gpus -1 --worker 4 vnl --pretrained 1 --learning_rate 1e-04 --batch_size 4 --encoder 'resnext101_32x4d_body_stride16' stdepth --path '/mnt/hdd/dome/STDepth_data2' --training --validation
-
