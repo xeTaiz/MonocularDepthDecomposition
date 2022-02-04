eb4753bbf12f1adb108bfe6c68f5ee6174c9cac3
Dominik Engel <dominik.engel@mail.de>
2022-02-04 09:21:11 +0100

    update train and submit scripts

--- submit_bts_all_methods.sh
+++ submit_bts_all_methods.sh
@@ -1,6 +1,4 @@
-ls -l
-pwd
-submit "./train_stdepth_bts.sh first_hit" --pytorch --requirements mono-depth-estimation/requirements.txt --name bts-firsthit --apt-install libsm6 libxext6 libxrender-dev libglib2.0-0 libgl1-mesa-glx git python3-opencv --exclude worker2
-submit "./train_stdepth_bts.sh max_opacity" --pytorch --requirements mono-depth-estimation/requirements.txt --name bts-maxopacity --apt-install libsm6 libxext6 libxrender-dev libglib2.0-0 libgl1-mesa-glx git python3-opencv --exclude worker2
-submit "./train_stdepth_bts.sh max_gradient" --pytorch --requirements mono-depth-estimation/requirements.txt --name bts-maxgradient --apt-install libsm6 libxext6 libxrender-dev libglib2.0-0 libgl1-mesa-glx git python3-opencv --exclude worker2
-submit "./train_stdepth_bts.sh wysiwyg" --pytorch --requirements mono-depth-estimation/requirements.txt --name bts-wysiwyg --apt-install libsm6 libxext6 libxrender-dev libglib2.0-0 libgl1-mesa-glx git python3-opencv --exclude worker2
+submit ./train_stdepth_bts_firsthit.sh  --pytorch --requirements mono-depth-estimation/requirements.txt --name bts-firsthit --apt-install libsm6 libxext6 libxrender-dev libglib2.0-0 libgl1-mesa-glx git python3-opencv --exclude worker2
+submit ./train_stdepth_bts_maxopacity.sh --pytorch --requirements mono-depth-estimation/requirements.txt --name bts-maxopacity --apt-install libsm6 libxext6 libxrender-dev libglib2.0-0 libgl1-mesa-glx git python3-opencv --exclude worker2
+submit ./train_stdepth_bts_maxgradient.sh --pytorch --requirements mono-depth-estimation/requirements.txt --name bts-maxgradient --apt-install libsm6 libxext6 libxrender-dev libglib2.0-0 libgl1-mesa-glx git python3-opencv --exclude worker2
+submit ./train_stdepth_bts_wysiwyg.sh --pytorch --requirements mono-depth-estimation/requirements.txt --name bts-wysiwyg --apt-install libsm6 libxext6 libxrender-dev libglib2.0-0 libgl1-mesa-glx git python3-opencv --exclude worker2
