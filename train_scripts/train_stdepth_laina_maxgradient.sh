58fb99ae22416251cbc7e8333be1b60cbfc41d3a
Dominik Engel <dominik.engel@mail.de>
2022-02-04 10:12:27 +0100

    add remaining train and submit scripts, restructure to train_scripts/

---
+++ train_scripts/train_stdepth_laina_maxgradient.sh
@@ -0,0 +1,1 @@
+python mono-depth-estimation/train.py --precision 32 --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 laina --learning_rate 0.0001 --batch_size 8 stdepth --depth-method max_gradient --path '/mnt/hdd/dome/STDepth_data3' --training --validation
---
+++ train_scripts/train_stdepth_midas_wysiwyp.sh
@@ -0,0 +1,1 @@
+python mono-depth-estimation/train.py --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 midas --learning_rate 1e-5 --batch_size 16 --loss mse --pretrained 0 stdepth --depth-method wysiwyp --path '/mnt/hdd/dome/STDepth_data3' --training --validation
