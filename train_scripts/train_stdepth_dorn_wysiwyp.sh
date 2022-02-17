23ac5facdc6540ca9fd2c09cd756838aca3a1445
Dominik Engel <dominik.engel@mail.de>
2022-02-17 09:18:57 +0100

    add wandb to train scripts

--- train_scripts/train_stdepth_dorn_wysiwyp.sh
+++ train_scripts/train_stdepth_dorn_wysiwyp.sh
@@ -1,1 +1,2 @@
+wandb login 2a9d65e893e7a362c9eb315f11d6826fcfb1a853
 python mono-depth-estimation/train.py --precision 32 --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 stdepth --depth-method wysiwyp --path "/mnt/hdd/dome/STDepth_data3" --training --validation dorn --learning_rate 0.0001 --batch_size 8 --pretrained 1
