970b37762aa28ebed28794c0af7fd0a1888547ef
Dominik Engel <dominik.engel@mail.de>
2022-02-21 08:16:43 +0100

    "stdepth_data3 -> 4"

--- train_scripts/train_stdepth_midas_maxgradient.sh
+++ train_scripts/train_stdepth_midas_maxgradient.sh
@@ -1,2 +1,2 @@
 wandb login 2a9d65e893e7a362c9eb315f11d6826fcfb1a853
-python mono-depth-estimation/train.py --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 midas --learning_rate 1e-5 --batch_size 16 --loss mse --pretrained 0 stdepth --depth-method max_gradient --path '/mnt/hdd/dome/STDepth_data3' --training --validation
+python mono-depth-estimation/train.py --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 midas --learning_rate 1e-5 --batch_size 16 --loss mse --pretrained 0 stdepth --depth-method max_gradient --path '/mnt/hdd/dome/STDepth_data4' --training --validation
