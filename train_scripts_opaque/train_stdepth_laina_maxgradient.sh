3dbc9a22d4b7d677429ff13d6e08782a5d724a6d
Dominik Engel <dominik.engel@uni-ulm.de>
2022-03-02 11:57:51 +0100

    train scripts for opaque dataset

---
+++ train_scripts_opaque/train_stdepth_laina_maxgradient.sh
@@ -0,0 +1,2 @@
+wandb login 2a9d65e893e7a362c9eb315f11d6826fcfb1a853
+python mono-depth-estimation/train.py --precision 32 --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 laina --learning_rate 0.0001 --batch_size 8 stdepth --depth-method max_gradient --path '/mnt/hdd/dome/STDepth_data4' --training --validation
