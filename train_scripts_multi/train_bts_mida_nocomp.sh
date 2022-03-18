6767a4f6a7e9168245e31f1b316122eaa8cbc679
Dominik Engel <dominik.engel@mail.de>
2022-03-18 16:56:01 +0100

    "add nocomp train scripts"

---
+++ train_scripts_multi/train_bts_mida_nocomp.sh
@@ -0,0 +1,2 @@
+wandb login 2a9d65e893e7a362c9eb315f11d6826fcfb1a853
+python -u monomulti-depth-estimation/train.py --precision 16 --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 bts --learning_rate 1e-5 --batch_size 12 --loss silog stdepthmulti --path '/mnt/hdd/dome/STDepth_data_multi_mida' --training --validation
