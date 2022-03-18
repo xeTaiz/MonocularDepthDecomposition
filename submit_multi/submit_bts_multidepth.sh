a6604fbb37f2f7f5b694aabeb93d73911a8d200c
Dominik Engel <dominik.engel@uni-ulm.de>
2022-03-18 15:20:12 +0100

    sort submit scripts

---
+++ submit_multi/submit_bts_multidepth.sh
@@ -0,0 +1,1 @@
+submit train_scripts/train_stdepth_bts_multi.sh --pytorch --requirements monomulti-depth-estimation/requirements.txt --name bts-multi --apt-install libsm6 libxext6 libxrender-dev libglib2.0-0 libgl1-mesa-glx git python3-opencv --exclude worker2 --himem
---
+++ submit_multi/submit_midas_multidepth_many.sh
@@ -0,0 +1,1 @@
+submit train_scripts/train_stdepth_midas_multi_many.sh --pytorch --requirements monomulti-depth-estimation/requirements.txt --name midas-multi-bg1 --apt-install libsm6 libxext6 libxrender-dev libglib2.0-0 libgl1-mesa-glx git python3-opencv --exclude worker2
