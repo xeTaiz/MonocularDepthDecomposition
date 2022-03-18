2f3d4a44496f244c8043ed3e91fdc17f1787722d
Dominik Engel <dominik.engel@mail.de>
2022-03-18 16:56:01 +0100

    "add nocomp train scripts"

--- train_scripts_multi/train_laina_wysiwyp.sh
+++ train_scripts_multi/train_laina_wysiwyp.sh
@@ -1,2 +1,2 @@
 wandb login 2a9d65e893e7a362c9eb315f11d6826fcfb1a853
-python monomulti-depth-estimation/train.py --precision 32 --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 laina --learning_rate 0.0001 --batch_size 8 --loss mae+composite stdepthmulti --path '/mnt/hdd/dome/STDepth_data_multi_wysiwyp' --training --validation
+python mono-depth-estimation/train.py --precision 32 --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 laina --learning_rate 0.0001 --batch_size 8 --loss mae+composite stdepthmulti --path '/mnt/hdd/dome/STDepth_data_multi_wysiwyp' --training --validation
