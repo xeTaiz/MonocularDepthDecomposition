8b470674205bc1738ae824ccba02293a5e780a84
Dominik Engel <dominik.engel@mail.de>
2022-03-19 11:06:48 +0100

    "update train scripts multi, add single layer scripts"

--- train_scripts_multi/train_laina_wysiwyp_nocomp.sh
+++ train_scripts_multi/train_laina_wysiwyp_nocomp.sh
@@ -1,2 +1,2 @@
 wandb login 2a9d65e893e7a362c9eb315f11d6826fcfb1a853
-python mono-depth-estimation/train.py --precision 32 --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 laina --learning_rate 0.0001 --batch_size 8 --loss mae stdepthmulti --path '/mnt/hdd/dome/STDepth_data_multi_wysiwyp' --training --validation
+python -u monomulti-depth-estimation/train.py --precision 32 --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 laina --learning_rate 0.0001 --batch_size 8 --loss mae stdepthmulti --path '/mnt/hdd/dome/STDepth_data_multi_wysiwyp' --training --validation
