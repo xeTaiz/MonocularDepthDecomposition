480ec2b1a1bec84f1bf44aaf5d7bd9d510f030a4
Dominik Engel <dominik.engel@mail.de>
2022-03-20 12:57:06 +0100

    add name to train scripts

--- train_scripts_multi/train_bts_wysiwyp_nocomp.sh
+++ train_scripts_multi/train_bts_wysiwyp_nocomp.sh
@@ -1,2 +1,2 @@
 wandb login 2a9d65e893e7a362c9eb315f11d6826fcfb1a853
-python -u monomulti-depth-estimation/train.py --precision 16 --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 bts --loss silog --learning_rate 1e-5 --batch_size 12 stdepthmulti --path '/mnt/hdd/dome/STDepth_data_multi_wysiwyp' --training --validation
+python -u monomulti-depth-estimation/train.py --name bts-mu2wys-nocomp --precision 16 --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 bts --loss silog --learning_rate 1e-5 --batch_size 12 stdepthmulti --path '/mnt/hdd/dome/STDepth_data_multi_wysiwyp' --training --validation
--- train_scripts_multi/train_laina_mida.sh
+++ train_scripts_multi/train_laina_mida.sh
@@ -1,2 +1,2 @@
 wandb login 2a9d65e893e7a362c9eb315f11d6826fcfb1a853
-python -u monomulti-depth-estimation/train.py --precision 32 --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 laina --learning_rate 0.0001 --batch_size 8 --loss mae+composite stdepthmulti --path '/mnt/hdd/dome/STDepth_data_multi_mida' --training --validation
+python -u monomulti-depth-estimation/train.py --name laina-mu2mida-comp --precision 32 --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 laina --learning_rate 0.0001 --batch_size 8 --loss mae+composite stdepthmulti --path '/mnt/hdd/dome/STDepth_data_multi_mida' --training --validation
