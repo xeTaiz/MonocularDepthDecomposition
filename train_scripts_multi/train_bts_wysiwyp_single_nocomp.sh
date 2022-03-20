3c1b72727007cc2b2f2d6ecda17c0a48172f8379
Dominik Engel <dominik.engel@mail.de>
2022-03-20 12:57:06 +0100

    add name to train scripts

--- train_scripts_multi/train_bts_wysiwyp_single_nocomp.sh
+++ train_scripts_multi/train_bts_wysiwyp_single_nocomp.sh
@@ -1,2 +1,2 @@
 wandb login 2a9d65e893e7a362c9eb315f11d6826fcfb1a853
-python -u monomulti-depth-estimation/train.py --precision 16 --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 bts --out-channels 10 --loss silog --learning_rate 1e-5 --batch_size 12 stdepthmulti --single-layer --path '/mnt/hdd/dome/STDepth_data_multi_wysiwyp' --training --validation
+python -u monomulti-depth-estimation/train.py --name bts-mu2wys-si-nocomp --precision 16 --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 bts --out-channels 10 --loss silog --learning_rate 1e-5 --batch_size 12 stdepthmulti --single-layer --path '/mnt/hdd/dome/STDepth_data_multi_wysiwyp' --training --validation
