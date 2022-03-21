72a54e63a9ffe216898f27c28ca3559a01d690a4
Dominik Engel <dominik.engel@mail.de>
2022-03-21 07:05:41 +0100

    add train and submit for MIDA opaque

---
+++ train_scripts_opaque/train_stdepth_dorn_mida.sh
@@ -0,0 +1,2 @@
+wandb login 2a9d65e893e7a362c9eb315f11d6826fcfb1a853
+python mono-depth-estimation/train.py --precision 32 --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 stdepth --depth-method mida --path "/mnt/hdd/dome/STDepth_data_opaque_mida" --training --validation dorn --learning_rate 0.0001 --batch_size 6 --pretrained 1
