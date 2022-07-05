1a17bac7c9408d88bc9cad8da5bdc2980d8f6cc6
Dominik Engel <dominik.engel@uni-ulm.de>
2022-07-05 07:46:40 +0200

    add train scripts and stuff

---
+++ train_scripts/train_stdepth_laina_multi.sh
@@ -0,0 +1,3 @@
+wandb login 2a9d65e893e7a362c9eb315f11d6826fcfb1a853
+python monomulti-depth-estimation/train.py --precision 32 --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 laina --learning_rate 0.0001 --batch_size 8 stdepthmulti --depth-method wysiwyp3 --path '/mnt/hdd/dome/STDepth_data_multi' --training --validation
+python monomulti-depth-estimation/train.py --precision 32 --min_epochs 10 --max_epochs 50 --gpus -1 --worker 4 laina --learning_rate 0.0001 --batch_size 8 stdepthmulti --background-depth-max --depth-method wysiwyp3 --path '/mnt/hdd/dome/STDepth_data_multi' --training --validation
